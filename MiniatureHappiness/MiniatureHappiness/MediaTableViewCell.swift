353756a546be2257f73035a8bf807407b504df7e
Roshan Mahanama <rmtreks@gmail.com>
2015-06-08 11:03:43 +1000

    basic project setup - retrieving count of photos and videos

---
+++ MiniatureHappiness/MiniatureHappiness/MediaTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MediaTableViewCell.swift
+//  MiniatureHappiness
+//
+//  Created by Roshan Mahanama on 8/06/2015.
+//  Copyright (c) 2015 RMTREKS. All rights reserved.
+//
+
+import UIKit
+
+class MediaTableViewCell: UITableViewCell {
+
+    @IBOutlet var mediaImage: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
